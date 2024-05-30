da43629c7e50cdf43c7969fc80b71c906e369cc7
adithyahemanth <kandalaadithya@gmail.com>
2024-05-30 23:52:12 +0530

    a

---
+++ tradesmooth-service/src/main/java/com/tradesmooth/tradesmooth/TradesmoothApplication.java
@@ -0,0 +1,13 @@
+package com.tradesmooth.tradesmooth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradesmoothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradesmoothApplication.class, args);
+	}
+
+}
---
+++ tradesmooth-service/src/test/java/com/tradesmooth/tradesmooth/TradesmoothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesmooth.tradesmooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradesmoothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
